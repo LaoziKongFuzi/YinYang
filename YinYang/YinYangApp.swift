ce573f28aeea1a7bb9a9dac43cee1de6350d2738
LaoziKongFuzi <142866425+LaoziKongFuzi@users.noreply.github.com>
2023-08-22 17:27:32 +1200

    Initial Commit

---
+++ YinYang/YinYangApp.swift
@@ -0,0 +1,17 @@
+//
+//  YinYangApp.swift
+//  YinYang
+//
+//  Created by JACK REEVE on 22/08/23.
+//
+
+import SwiftUI
+
+@main
+struct YinYangApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
